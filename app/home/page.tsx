1568dfa601209dc6fde0ca9f9a0d696c0aa40afe
yunsung <12345ggh81@gmail.com>
2026-01-25 21:36:45 +0900

    UPDATE: 필요없는 라인 삭제 및 주석 업데이트

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,9 @@
+import CrossTitch from "../src/components/CrossTitch";
+
 export default async function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <CrossTitch />
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,11 @@
 
 // 1. 깃허브 로그인 v
 // 2. 깃허브 커밋수 가져올 수 있게 v
-// 3. 커밋수에 따라 해당 사용자가 십자수 박을 수 있게
+// 3. 커밋수에 따라 해당 사용자가 십자수 박을 수 있게 / TODO 십자수 구현 완 -> 커밋수 적용
 // 4. 박은 십자수를 svg 형식으로 바꾼 후 링크 부여
 // 5. 해당 링크를 리드미에 박으면 내가 쓴 십자수가 만들어짐
 
 // if 십자수를 이전에 박은적 있다면 그걸 가져오게 만들어야함
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 export default function Home() {
   return (
